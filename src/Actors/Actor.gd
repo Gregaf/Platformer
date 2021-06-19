9b7a6009cb05f700edec967b89c7b09468711759
Gregory Freitas <gregfreitas1997@gmail.com>
2021-06-19 13:06:47 -0400

    Stomping, Camera and Signals

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -6,6 +6,6 @@ const FLOOR_NORMAL: = Vector2.UP
 export var maxSpeed = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0
 
-var velocity = Vector2.ZERO
+var _velocity = Vector2.ZERO
 
 	
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,5 +1,7 @@
 extends Actor
 
+export var stomp_impulse = 1000.0
+
 var direction: = Vector2.ZERO
 
 
@@ -7,18 +9,29 @@ var direction: = Vector2.ZERO
 func _ready():
 	pass # Replace with function body.
 
+func _on_EnemyDetector_area_entered(area) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+func _on_EnemyDetector_body_entered(body) -> void:
+	queue_free()
+
 func _physics_process(delta: float) -> void:
-	var is_jump_interrupted = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	direction = get_direction()
 	
-	velocity = calculate_move_velocity(velocity, direction, maxSpeed, is_jump_interrupted)
-	move_and_slide(velocity, FLOOR_NORMAL)
+	_velocity = calculate_move_velocity(_velocity, direction, maxSpeed, is_jump_interrupted)
+	move_and_slide(_velocity, FLOOR_NORMAL)
 	
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0		
 	)
+
+func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
+	var out = linear_velocity
+	out.y = -impulse
+	return out
 
 func calculate_move_velocity(
 		linear_velocity: Vector2,
